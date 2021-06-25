e8de6bf2be4a4f374a0cd454559eb932cc5cb337
Matthew W <fatboychummy@gmail.com>
2021-06-25 02:09:40 -0600

    Add numworker handling

--- Update.lua
+++ Update.lua
@@ -1,7 +1,9 @@
 local args = table.pack(...)
+local numWorkers = tonumber(args[1]) or 4
+
 local selfDir = fs.getDir(shell.getRunningProgram())
 
 local Updater = require "Updater"
 
 local data = Updater.GetData()
-Updater.DownloadFiles(data, 4)
+Updater.DownloadFiles(data, numWorkers)
