9e3b86c4a700484a221f8361c944224c538d6a9a
Matthew W <fatboychummy@gmail.com>
2022-09-04 02:30:24 -0600

    Add initial focusable coroutine manager. This will need more testing in future.

--- Utilities.lua
+++ Utilities.lua
@@ -12,4 +12,53 @@ function Utilities.Lerp(a, b, alpha)
   return a * (1-alpha) + b * alpha
 end
 
+--- Please note that the focused coroutine should NEVER stop, instead when
+--- control needs to change, it should be set to nil.
+---@return function Manager The coroutine manager.
+---@return table Focus The sharable focus object, set Focus[1] to change the thread used.
+function Utilities.FocusableCoroutine()
+  local focus = {}
+
+  return function()
+    local coro ---@type thread?
+    local waiting ---@type string?
+
+    --- Resume a coroutine and set what filter we are waiting for
+    local function resume(...)
+      ---@diagnostic disable-next-line This is only called if coro is not nil.
+      local ok, _waiting = coroutine.resume(coro, ...)
+      waiting = _waiting
+
+      -- If the coroutine stopped or errored...
+      if not ok then
+        if waiting == "cannot resume dead coroutine" then
+          error("Focused coroutine stopped unexpectedly.", 0)
+        end
+        error(waiting, 0)
+      end
+    end
+
+    -- Main loop: Pull event, resume coroutine.
+    while true do
+      local event = table.pack(os.pullEvent())
+
+      if event[1] == "coroutine_focus" then
+        coro = focus[1]
+        waiting = nil
+        if coro then
+          resume()
+        end
+      else
+        if coro and (waiting == nil or waiting == event[1] or event[1] == "terminate") then
+          resume(table.unpack(event, 1, event.n))
+        end
+      end
+
+      if coro and coroutine.status(coro) == "dead" then
+        error("Focused coroutine stopped unexpectedly.", 0)
+      end
+    end
+  end, focus
+end
+
 return Utilities
