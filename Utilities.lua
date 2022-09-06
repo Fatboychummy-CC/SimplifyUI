e3c47035156f3439e89571e055d2dbfd18fb7150
Matthew W <fatboychummy@gmail.com>
2022-09-05 23:33:11 -0600

    EditableCoroutine manager.

--- Utilities.lua
+++ Utilities.lua
@@ -19,8 +19,8 @@ end
 --- 
 --- Please note that the focused thread should NEVER stop, instead when control
 --- needs to change, it should be set to nil or the other thread.
----@return function Manager The coroutine manager.
----@return table Focus The sharable focus object, set Focus[1] to change the thread used.
+---@return function manager The coroutine manager.
+---@return table focus The sharable focus object, set Focus[1] to change the thread used.
 function Utilities.FocusableCoroutine()
   local focus = {}
 
@@ -36,9 +36,6 @@ function Utilities.FocusableCoroutine()
 
       -- If the coroutine stopped or errored...
       if not ok then
-        if waiting == "cannot resume dead coroutine" then
-          error("Focused coroutine stopped unexpectedly.", 0)
-        end
         error(waiting, 0)
       end
     end
@@ -64,6 +61,79 @@ function Utilities.FocusableCoroutine()
       end
     end
   end, focus
+end
+
+--- Returns a function which can be used in parallel to run multiple coroutines
+--- as needed. Two other functions are returned that allow you to add and remove
+--- coroutines whenever needed.
+---
+---@return function manager The coroutine manager.
+---@return fun(name:string, func:fun()|thread) coroutine_add The function that adds coroutines.
+---@return fun(name:string) coroutine_remove The function that removes coroutines.
+function Utilities.EditableCoroutine()
+  local coroutines = {}
+  local filters = {}
+
+  --- Adds a coroutine to be run in the manager.
+  ---@param name string The name of the coroutine.
+  ---@param func fun()|thread The function to be converted to a coroutine, or a thread.
+  local function coroutine_add(name, func)
+    expect(1, name, "string")
+    expect(2, func, "function", "thread")
+
+    if type(func) == "function" then
+      func = coroutine.create(func)
+    end
+
+    coroutines[name] = func
+  end
+
+  --- Removes a coroutine from the manager.
+  ---@param name string The name of the coroutine to be removed.
+  local function coroutine_remove(name)
+    coroutines[name] = nil
+    filters[name] = nil
+  end
+
+  --- Coroutine manager.
+  local function manager()
+    ---@param coro thread The coroutine to resume.
+    ---@param name string The coroutine's name.
+    ---@param ... any The values to resume with.
+    local function resume(coro, name, ...)
+      local ok, filter = coroutine.resume(coro, ...)
+      
+      -- If the coroutine stopped due to error, throw the error.
+      if not ok then
+        error(filter, 0)
+      end
+
+      filters[name] = filter
+    end
+
+    -- Main coroutine loop
+    while true do
+      -- Gather the event.
+      local event = table.pack(os.pullEvent())
+
+      -- Loop through each coroutine and check if it should be resumed.
+      for name, coro in pairs(coroutines) do
+        -- If filter is the same
+        -- Or if filter is not set (take any event)
+        -- Or if the event is a terminate event
+        if event[1] == filters[name] or event[1] == "terminate" or not filters[name] then
+          resume(coro, name, table.unpack(event, 1, event.n))
+
+          -- If the coroutine finished, remove it.
+          if coroutine.status(coro) == "dead" then
+            coroutine_remove(name)
+          end
+        end
+      end
+    end
+  end
+
+  return manager, coroutine_add, coroutine_remove
 end
 
 --- Deep-clones a table.
